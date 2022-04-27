3c02cc33069dec3a920673114e172cbe1bfec046
Filip U <filipurbaniak.1999@gmail.com>
2022-04-27 09:04:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
-import pandas
 
 streamlit.title("My Mom's new healthy dinner") 
-
 streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,8 +10,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.mutliselect("Pick some fruit's: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
